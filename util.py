23edd78c75036d20ab581ac00fc376c9f312b549
Tyler Christeson <christesont@gmail.com>
2021-04-12 13:33:43 -0600

    updated default power

--- util.py
+++ util.py
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
-def util(n, power =2):
+def util(n, fn = lambda x: x**3):
 
     '''
     calculates power for number up to n
     args:
-        power: power for numbers to raise, default power is 2
+        power: power for numbers to raise, default power is 3
         n: highest number in list of numbers to raise, no default
     '''
-    return [i**power for i in range(n)]
-=======
-def util(n, fn = lambda x: x**2):
-
-    #changes
     return [fn(i) for i in range(n)]
->>>>>>> apply_lambda_list
 
 print(util(10))
